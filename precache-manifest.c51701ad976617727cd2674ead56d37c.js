efa723bef89c3587647f89457881cbe2ab20047c
akhilpatlolla <patlolla.akhil@gmail.com>
2018-10-04 11:11:32 -0400

    Updates

---
+++ precache-manifest.c51701ad976617727cd2674ead56d37c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Generic_ETL_Utility_AWS_GLUE/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6684a20e1cdae0f87c99",
+    "url": "/Generic_ETL_Utility_AWS_GLUE/static/js/runtime~main.6684a20e.js"
+  },
+  {
+    "revision": "2911cbd2adc988153965",
+    "url": "/Generic_ETL_Utility_AWS_GLUE/static/js/main.2911cbd2.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/Generic_ETL_Utility_AWS_GLUE/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "2911cbd2adc988153965",
+    "url": "/Generic_ETL_Utility_AWS_GLUE/static/css/main.36c7ad09.chunk.css"
+  },
+  {
+    "revision": "6fe403178c5d60269c6c3c14d0507a0f",
+    "url": "/Generic_ETL_Utility_AWS_GLUE/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/Generic_ETL_Utility_AWS_GLUE/precache-manifest.c51701ad976617727cd2674ead56d37c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Generic_ETL_Utility_AWS_GLUE/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
